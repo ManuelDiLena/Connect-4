44bcc03031f0b0e15693ca545e38c4d12e331263
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-02-04 17:08:07 +0100

    Main component with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import LOGO from './assets/main-logo.png';
+import CPUICON from './assets/cpu-icon.png';
+import PLAYERICON from './assets/player-icon.png';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='main__container buttons__container'>
+      <img src={LOGO} className='main__logo' alt='logo' />
+      <button className='main__button button__pink'>
+        PLAY vs CPU
+        <img src={CPUICON} className='button__icon cpu-icon' alt='cpu icon' />
+      </button>
+      <button className='main__button button__yellow'>
+        PLAY vs PLAYER
+        <img src={PLAYERICON} className='button__icon player-icon' alt='player icon' />
+      </button>
+      <button className='main__button button__light'>
+        GAME RULES
+      </button>
     </div>
   );
 }
